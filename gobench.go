39783e7a6db7371b9f5aad69a03f709585969818
cmpxchg16 <cmpxchg16@gmail.com>
2015-09-15 16:57:00 +0300

    revert all pull requests due to failures

--- gobench.go
+++ gobench.go
@@ -4,54 +4,42 @@ import (
 	"bufio"
 	"bytes"
 	"crypto/tls"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"runtime"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 )
 
 var (
-	requests           int64
-	period             int64
-	clients            int
-	url                string
-	urlsFilePath       string
-	keepAlive          bool
-	postDataFilePath   string
-	connectTimeout     int
-	writeTimeout       int
-	readTimeout        int
-	authHeader         string
-	lowerStatusSuccess int
-	upperStatusSuccess int
-)
-
-const (
-	maxExpandNum = 1000
+	requests         int64
+	period           int64
+	clients          int
+	url              string
+	urlsFilePath     string
+	keepAlive        bool
+	postDataFilePath string
+	connectTimeout   int
+	writeTimeout     int
+	readTimeout      int
+	authHeader       string
 )
 
 type Configuration struct {
-	urls               []string
-	method             string
-	postData           []byte
-	requests           int64
-	period             int64
-	keepAlive          bool
-	authHeader         string
-	lowerStatusSuccess int
-	upperStatusSuccess int
+	urls       []string
+	method     string
+	postData   []byte
+	requests   int64
+	period     int64
+	keepAlive  bool
+	authHeader string
 }
 
 type Result struct {
@@ -95,14 +83,7 @@ func (this *MyConn) Write(b []byte) (n int, err error) {
 func init() {
 	flag.Int64Var(&requests, "r", -1, "Number of requests per client")
 	flag.IntVar(&clients, "c", 100, "Number of concurrent clients")
-	flag.StringVar(&url, "u", "", fmt.Sprintf(`URL. Support expression such as {S10,1-100}  {R14,2-9}.
-            S or s means sequence number between [1,100] , the amount is 10, as specified in the expression.
-            R or r means random number between [2,9] , the amount is 14. 
-            We will generate at most %d urls one time, for the security reason.
-            For example : http://www.qq.com/?{s3,1-10} will produce 3 lines as follow
-            http://www.qq.com?1
-            http://www.qq.com?2
-            http://www.qq.com?3 `, maxExpandNum))
+	flag.StringVar(&url, "u", "", "URL")
 	flag.StringVar(&urlsFilePath, "f", "", "URL's file path (line seperated)")
 	flag.BoolVar(&keepAlive, "k", true, "Do HTTP keep-alive")
 	flag.StringVar(&postDataFilePath, "d", "", "HTTP POST data file path")
@@ -111,8 +92,6 @@ func init() {
 	flag.IntVar(&writeTimeout, "tw", 5000, "Write timeout (in milliseconds)")
 	flag.IntVar(&readTimeout, "tr", 5000, "Read timeout (in milliseconds)")
 	flag.StringVar(&authHeader, "auth", "", "Authorization header")
-	flag.IntVar(&lowerStatusSuccess, "ls", 200, "Lower success status code")
-	flag.IntVar(&upperStatusSuccess, "us", 299, "Upper success status code")
 }
 
 func printResults(results map[int]*Result, startTime time.Time) {
@@ -132,19 +111,21 @@ func printResults(results map[int]*Result, startTime time.Time) {
 		writeThroughput += result.writeThroughput
 	}
 
-	elapsed := time.Since(startTime).Seconds()
+	elapsed := int64(time.Since(startTime).Seconds())
+
+	if elapsed == 0 {
+		elapsed = 1
+	}
 
 	fmt.Println()
-	fmt.Printf("Requests:                       %13d hits\n", requests)
-	fmt.Printf("Successful requests:            %13d hits\n", success)
-	fmt.Printf("Network failed:                 %13d hits\n", networkFailed)
-	fmt.Printf("Bad requests failed (!2xx):     %13d hits\n", badFailed)
-	fmt.Printf("Successfull requests rate:      %13.2f hits/sec\n", float64(success)/elapsed)
-	fmt.Printf("Read transferred:               %13d bytes\n", readThroughput) // add to contrast it with ab
-	fmt.Printf("Write transferred:              %13d bytes\n", writeThroughput)
-	fmt.Printf("Read throughput:                %13.2f bytes/sec\n", float64(readThroughput)/elapsed)
-	fmt.Printf("Write throughput:               %13.2f bytes/sec\n", float64(writeThroughput)/elapsed)
-	fmt.Printf("Test time:                      %13.2f sec\n", elapsed)
+	fmt.Printf("Requests:                       %10d hits\n", requests)
+	fmt.Printf("Successful requests:            %10d hits\n", success)
+	fmt.Printf("Network failed:                 %10d hits\n", networkFailed)
+	fmt.Printf("Bad requests failed (!2xx):     %10d hits\n", badFailed)
+	fmt.Printf("Successful requests rate:       %10d hits/sec\n", success/elapsed)
+	fmt.Printf("Read throughput:                %10d bytes/sec\n", readThroughput/elapsed)
+	fmt.Printf("Write throughput:               %10d bytes/sec\n", writeThroughput/elapsed)
+	fmt.Printf("Test time:                      %10d sec\n", elapsed)
 }
 
 func readLines(path string) (lines []string, err error) {
@@ -196,14 +177,12 @@ func NewConfiguration() *Configuration {
 	}
 
 	configuration := &Configuration{
-		urls:               make([]string, 0),
-		method:             "GET",
-		postData:           nil,
-		keepAlive:          keepAlive,
-		requests:           int64((1 << 63) - 1),
-		authHeader:         authHeader,
-		lowerStatusSuccess: lowerStatusSuccess,
-		upperStatusSuccess: upperStatusSuccess}
+		urls:       make([]string, 0),
+		method:     "GET",
+		postData:   nil,
+		keepAlive:  keepAlive,
+		requests:   int64((1 << 63) - 1),
+		authHeader: authHeader}
 
 	if period != -1 {
 		configuration.period = period
@@ -241,7 +220,7 @@ func NewConfiguration() *Configuration {
 	}
 
 	if url != "" {
-		configuration.urls = append(configuration.urls, expandUrl(url)...)
+		configuration.urls = append(configuration.urls, url)
 	}
 
 	if postDataFilePath != "" {
@@ -282,91 +261,8 @@ func MyClient(result *Result, connectTimeout, readTimeout, writeTimeout time.Dur
 			Dial:              TimeoutDialer(result, connectTimeout, readTimeout, writeTimeout),
 			TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
 			DisableKeepAlives: !keepAlive,
-			Proxy:             http.ProxyFromEnvironment,
 		},
 	}
-}
-
-func expression_template_1(e []byte) (n, from, to int, err error) {
-	n, from, to, err = 0, 0, 0, nil
-	if e == nil || len(e) < 6 {
-		err = errors.New("invalid url expression: length not enough")
-		return
-	}
-	if bytes.IndexAny(e[0:1], "SsRr") == 0 {
-		comma_cutted := bytes.SplitN(e[1:], []byte{','}, 2)
-		if comma_cutted == nil || len(comma_cutted) < 2 {
-			err = errors.New("invalid url expression: no enough comma seperated fields")
-			return
-		}
-		if n, err = strconv.Atoi(string(comma_cutted[0])); err != nil {
-			return
-		}
-		dash_cutted := bytes.SplitN(comma_cutted[1], []byte{'-'}, 2)
-		if dash_cutted == nil || len(dash_cutted) < 2 {
-			err = errors.New("invalid url expression: range invalid")
-			return
-		}
-		if from, err = strconv.Atoi(string(dash_cutted[0])); err != nil {
-			return
-		}
-		if to, err = strconv.Atoi(string(dash_cutted[1])); err != nil {
-			return
-		}
-		return
-	} else {
-		err = errors.New("invalid url expression: expression not suit for the template1")
-		return
-	}
-}
-
-func genValue(e []byte) (values []string) {
-	values = nil
-	if len(e) == 0 {
-		return
-	}
-	switch e[0] {
-	case 'S', 's':
-		// sequence
-		n, from, to, err := expression_template_1(e)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for i := from; i <= to && i-from < n && i-from < maxExpandNum; i++ {
-			values = append(values, fmt.Sprintf("%d", i))
-		}
-	case 'R', 'r':
-		// random
-		n, from, to, err := expression_template_1(e)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for i := 0; i < n && i < maxExpandNum; i++ {
-			values = append(values, fmt.Sprintf("%d", rand.Intn(to-from+1)+from))
-		}
-	default:
-		return
-	}
-	return
-}
-
-// expand the {....} symbol to a value list
-// For now, only support sequence expand e.g. {S1,99} or {1,99} ;  rand expand {R1,99} ;
-func expandUrl(u string) (ret_urls []string) {
-	ret_urls = nil
-	url := []byte(u)
-	leftBracket, rightBracket := bytes.IndexByte(url, '{'), bytes.IndexByte(url, '}')
-	if leftBracket > 4 && rightBracket > 4 && leftBracket < rightBracket {
-		expanded_value := genValue(url[leftBracket+1 : rightBracket])
-		for i, gen := range expanded_value {
-			ret_urls = append(ret_urls,
-				strings.Replace(u, u[leftBracket:rightBracket+1], gen, 1))
-			fmt.Printf("debug: gen url [%d]:%s\n", i, ret_urls[len(ret_urls)-1])
-		}
-	} else {
-		ret_urls = append(ret_urls, u)
-	}
-	return
 }
 
 func client(configuration *Configuration, result *Result, done *sync.WaitGroup) {
@@ -403,24 +299,14 @@ func client(configuration *Configuration, result *Result, done *sync.WaitGroup) 
 				continue
 			}
 
-			//_, errRead := ioutil.ReadAll(resp.Body)
-			// use a memory efficient way
-			_m := make([]byte, 1024)
-			var errRead error = nil
-			for errRead == nil {
-				_, errRead = resp.Body.Read(_m)
-			}
-			if errRead == io.EOF {
-				errRead = nil
-			}
+			_, errRead := ioutil.ReadAll(resp.Body)
 
 			if errRead != nil {
 				result.networkFailed++
 				continue
 			}
 
-			if resp.StatusCode >= configuration.lowerStatusSuccess &&
-				resp.StatusCode <= configuration.upperStatusSuccess {
+			if resp.StatusCode == http.StatusOK {
 				result.success++
 			} else {
 				result.badFailed++
